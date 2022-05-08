2e50003bb41bd58f9914314e3440f0ba787ab716
Les-Triso <105150145+Les-Triso@users.noreply.github.com>
2022-05-08 11:26:01 +0200

    Add files via upload

---
+++ Agora/DATAS/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+//CONNEXION À LA BDD : CONSTANTES VIDES PAR DEFAUT!
+define("db_host", "localhost");
+define("db_login", "lamp");
+define("db_password", 'admin');
+define("db_name", "test");
+
+//ESPACE DISQUE / NB USERS / SALT
+define("limite_espace_disque", "10737418240");
+define("limite_nb_users", "10000");
+define("AGORA_SALT", "");
